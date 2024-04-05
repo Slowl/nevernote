679c9732a2b2a2d4b8ad8dafb4f6d479cdad41c1
slowl <hedi0475@gmail.com>
2024-04-05 10:53:59 +0200

    feat: enable PWA and add assets needed

--- vite.config.ts
+++ vite.config.ts
@@ -2,10 +2,41 @@ import { defineConfig } from 'vite'
 import path from 'path'
 import react from '@vitejs/plugin-react'
 import wyw from '@wyw-in-js/vite'
+import { VitePWA } from 'vite-plugin-pwa'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react(), wyw()],
+	plugins: [
+		react(),
+		wyw(),
+		VitePWA({
+			registerType: 'autoUpdate',
+			includeAssets: ['favicon.ico', 'apple-touch-icon-180x180.png', 'maskable-icon-512x512.svg'],
+      manifest: {
+        name: 'Nevernote',
+        short_name: 'Nevernote',
+        description: 'Best and simplest note taking app',
+        theme_color: '#1c1c1c',
+        icons: [
+          {
+            src: 'nevernote-64x64.png',
+            sizes: '64x64',
+            type: 'image/png'
+          },
+          {
+            src: 'nevernote-192x192.png',
+            sizes: '192x192',
+            type: 'image/png'
+          },
+          {
+            src: 'nevernote-512x512.png',
+            sizes: '512x512',
+            type: 'image/png'
+          }
+        ]
+      }
+		})
+	],
 	resolve: {
 		alias: { '@': path.resolve(__dirname, './src') }
 	}

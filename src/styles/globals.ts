ef27d50e8ac5ce622e44db9791399c9026611dcd
slowl <hedi0475@gmail.com>
2024-08-29 02:13:47 +0200

    chores: update dependencies

--- src/styles/globals.ts
+++ src/styles/globals.ts
@@ -3,6 +3,9 @@ import { css } from '@linaria/core'
 export const globals = css`
 	:global() {
 		@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300&display=swap');
+		@view-transition {
+			navigation: auto;
+		}
 		*, *::before, *::after { box-sizing: border-box; }
 		* { margin: 0; }
 		html {
